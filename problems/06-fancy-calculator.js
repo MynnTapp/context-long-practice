10f26988bac3efcb4977fdd65efe30822657b0a4
desmynn <desmynnmjc@gmail.com>
2024-07-24 17:22:20 -0500

    third changes

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,10 +1,38 @@
-// Your code here 
+// Your code here
+
+class Calculator {
+  constructor() {
+    this.total = 0;
+  }
+
+  add(num) {
+    this.total += num;
+    return this.total;
+  }
+}
+
+class FancyCalculator extends Calculator {
+  setTotal(newTotal) {
+    this.total = newTotal;
+    return this.total;
+  }
+
+  squared() {
+    this.total *= this.total;
+    return this.total;
+  }
+
+  modulo(num) {
+    this.total %= num;
+    return this.total;
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = FancyCalculator;
+  module.exports = FancyCalculator;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
