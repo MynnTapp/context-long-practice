2bc0b3978793116d4e30a41f48bac7bb5313148a
desmynn <desmynnmjc@gmail.com>
2024-07-24 14:52:28 -0500

    test

---
+++ problems/03-make-dog.js
@@ -0,0 +1,10 @@
+"jello";
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+  module.exports = Dog;
+} catch {
+  module.exports = null;
+}
